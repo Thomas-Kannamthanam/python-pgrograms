43c8803c91405bfa80542a0aa1cb79ed678a5c3e
Thomas-Kannamthanam <121820474+Thomas-Kannamthanam@users.noreply.github.com>
2024-02-19 22:38:32 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,11 +1,13 @@
-x=float(input("Enter the investment:"))
-y=int(input("Enter the number of years:"))
-z=int(input("Enter the rate as %:"))
-print("Year\t\tStarting balance\t\tInterest\t\tEnding balance")
-
-for num in range(0,y):
-    i=(x*z)/100
-    n=i+x
-    print(num,"\t\t",x,"\t\t\t",i,"\t\t\t",n)
-    x=n
-    
+starting_balance=float(input("Enter the investment amount: "))
+years=int(input("Enter the number of years: "))
+rate=float(input("Enter the rate as a %: "))/100
+total_interest=0.0
+print("Year\tStarting Balance\tInterest\tEnding Balance")
+for i in range(1,years+1):
+    interest=starting_balance*rate
+    ending_balance=starting_balance+interest
+    print(i,"\t","%0.2f"%starting_balance,"\t\t","%0.2f"%interest,"\t","%0.2f"%ending_balance)
+    total_interest+=interest
+    starting_balance=ending_balance
+print("Ending balance: ","%0.2f"%ending_balance)
+print("Total interest earned: ","%0.2f"%total_interest)
